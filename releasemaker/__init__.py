cb43bdd5db6577d0c47370a24c0bb07a2175eea7
Dan Loewenherz <dan@lionheartsw.com>
2016-07-16 17:43:29 -0500

    update version data

---
+++ releasemaker/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from .metadata import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+)
+
+from releasemaker import ReleaseMaker
+
+__all__ = [
+    '__author__',
+    '__copyright__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+    'ReleaseMaker'
+]
